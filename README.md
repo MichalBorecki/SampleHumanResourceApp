eaacdacb1ae02681b82fd321f4f6517bbef68234
Michał Borecki <michal.borecki.mb@gmail.com>
2019-11-20 21:04:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SampleHumanResourceApp
+Sample Human Resource web application created with Spring Boot and Vaadin
---
+++ src/test/java/pl/borecki/sampleHumanResourceApp/SampleHumanResourceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.borecki.sampleHumanResourceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleHumanResourceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
