b8029aede74795748fae529b283ce4f82b1cd974
Michał Borecki <michal.borecki.mb@gmail.com>
2019-11-26 23:51:42 +0100

    Add main view

---
+++ src/main/java/pl/borecki/sampleHumanResourceApp/gui/MainView.java
@@ -0,0 +1,8 @@
+package pl.borecki.sampleHumanResourceApp.gui;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route(layout = MainLayout.class)
+public class MainView extends VerticalLayout {
+}
